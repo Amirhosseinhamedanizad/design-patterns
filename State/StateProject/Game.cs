b82e42076cd07834560ba4ab2febef818772a27b
git1362 <alirezamoghadamzadeh@gmail.com>
2021-02-02 19:55:26 -0500

    Memento and State Design Pattern Session

---
+++ State/StateProject/Game.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StateProject
+{
+    //State
+    public interface State
+    {
+        void Pause();
+        void Stop();
+        void Play();
+    }
+
+    //Playing Concerete Class
+    public class PlayingState : State
+    {
+        private GameConsole _game;
+        public PlayingState(GameConsole game)
+        {
+            _game = game;
+        }
+        public void Pause()
+        {
+            _game.SetState(_game.getPausedgState());
+            Console.WriteLine("Pausing");
+        }
+
+        public void Play()
+        {
+            Console.WriteLine("Already Playing");
+        }
+
+        public void Stop()
+        {
+            _game.SetState(_game.getStoppedState());
+            Console.WriteLine("Stopping");
+        }
+    }
+
+    //Puased Concerete Class
+    public class PausedState : State
+    {
+        private GameConsole _game;
+        public PausedState(GameConsole game)
+        {
+            _game = game;
+        }
+
+        public void Pause()
+        {
+            Console.WriteLine("Already Paused");
+        }
+
+        public void Play()
+        {
+            _game.SetState(_game.getPlayingState());
+            Console.WriteLine("Playing");
+        }
+
+        public void Stop()
+        {
+            _game.SetState(_game.getStoppedState());
+            Console.WriteLine("Stopping");
+        }
+    }
+
+
+    //Stopped Concerete Class
+    public class StoppedState : State
+    {
+        private GameConsole _game;
+        public StoppedState(GameConsole game)
+        {
+            _game = game;
+        }
+
+        public void Pause()
+        {
+            Console.WriteLine("not possible to do this action");
+        }
+
+        public void Play()
+        {
+            _game.SetState(_game.getPlayingState());
+            Console.WriteLine("Playing");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Already Stopped");
+        }
+    }
+}
---
+++ State/StateProject/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace StateProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //problem
+            //if(textbox.string == "valid email") {
+            // checkbox.enabled = true;
+            // button.enabled = true;
+            //} else {
+            // checkbox.enabled = false;
+            // button.enabled = false;
+            //} else if(textbox.string == "valid email"){
+            // checkbox.enabled = true;
+            // button.enabled = true;
+            //textbox.enabled = false;
+            //} else (textbox.string == "valid email" && textbox.string.endof("org"){
+            // checkbox.enabled = true;
+            // button.enabled = true;
+            //textbox.enabled = false;
+            //}
+
+            //solution
+            // play -> pause -> stop -> play
+            Console.WriteLine("Welcome to XBox new Game Console");
+            Console.WriteLine("Running Your Game ...");
+
+            GameConsole console = new GameConsole();
+            //console.SetState(new PlayingState(console));
+            ////console.Play();
+            //console.Pause();
+            //console.Stop();
+
+            console.SetState(new StoppedState(console));
+            //console.Play();
+            console.Pause();
+            console.Stop();
+            Console.ReadKey();
+        }
+    }
+}

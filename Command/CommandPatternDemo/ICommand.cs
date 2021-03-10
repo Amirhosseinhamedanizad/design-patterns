6dc868bed9daa8e909f01d8d85da98a77e94e8d0
git1362 <alirezamoghadamzadeh@gmail.com>
2021-03-09 19:28:42 -0500

    Command Session

---
+++ Command/CommandPatternDemo/ICommand.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace CommandPatternDemo
+{
+    public interface ICommand
+    {
+    }
+
+    public class TransferCommand : ICommand
+    {
+        public string FromPerson { get; set; }
+        public string ToPerson { get; set; }
+        public decimal Amount { get; set; }
+    }
+
+    public class WithdrawCommand : ICommand
+    {
+        public string Person { get; set; }
+        public decimal Amount { get; set; }
+    }
+
+    //This is not an acceptable command
+    //public class FakeCommand
+    //{
+    //    public string FromPerson { get; set; }
+    //    public string ToPerson { get; set; }
+    //    public decimal Amount { get; set; }
+    //}
+
+    public interface ICommandHandler<TCommand> where TCommand : ICommand
+    {
+        void Handle(TCommand command);
+    }
+
+    public class TransferCommandHandler : ICommandHandler<TransferCommand>
+    {
+        private IBankingRepository _repository;
+        public TransferCommandHandler(IBankingRepository repository)
+        {
+            //definisve programming
+            _repository = repository ?? throw new ArgumentNullException(nameof(IBankingRepository));
+        }
+        public void Handle(TransferCommand command)
+        {
+            _repository.Transfer(command.FromPerson, command.ToPerson, command.Amount);
+        }
+    }
+
+    public class WithdrawCommandHandler : ICommandHandler<WithdrawCommand>
+    {
+        private IBankingRepository _repository;
+        public WithdrawCommandHandler(IBankingRepository repository)
+        {
+            //definisve programming
+            _repository = repository ?? throw new ArgumentNullException(nameof(IBankingRepository));
+        }
+        public void Handle(WithdrawCommand command)
+        {
+            _repository.Withdraw(command.Person, command.Amount);
+        }
+    }
+}
---
+++ Command/CommandPatternDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CommandPatternDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("From Person:");
+            string from = Console.ReadLine();
+
+            Console.WriteLine("To Person:");
+            string to = Console.ReadLine();
+
+            Console.WriteLine("How much ?");
+            decimal amount = Convert.ToDecimal(Console.ReadLine());
+
+            var transferCommand = new TransferCommand { FromPerson = from, ToPerson = to, Amount = amount };
+
+            new TransferCommandHandler(new BankingRepository()).Handle(transferCommand);
+
+            Console.WriteLine("Person:");
+            string person = Console.ReadLine();
+
+            Console.WriteLine("How much ?");
+            amount = Convert.ToDecimal(Console.ReadLine());
+
+            var withdrawCommand = new WithdrawCommand { Person = person, Amount = amount };
+            new WithdrawCommandHandler(new BankingRepository()).Handle(withdrawCommand);
+            Console.ReadLine();
+        }
+    }
+}

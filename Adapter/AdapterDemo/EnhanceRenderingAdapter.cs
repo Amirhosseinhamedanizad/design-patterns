4a7271a399ccc02e54eee369d862ef0e9111dda2
git1362 <alirezamoghadamzadeh@gmail.com>
2021-03-26 21:43:40 -0400

    Adapter Session Demo

---
+++ Adapter/AdapterDemo/EnhanceRenderingAdapter.cs
@@ -0,0 +1,25 @@
+﻿using AdapterDemo.ThirdPartyLibraries;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdapterDemo
+{
+    //adapter : incompatible interfaces connect together
+    public class EnhanceRenderingAdapter : IInternalReportingTool 
+    {
+        private readonly IThridPartyReportingTool _thridPartyReportingTool;
+
+        public EnhanceRenderingAdapter(IThridPartyReportingTool thridPartyReportingTool)
+        {
+            _thridPartyReportingTool = thridPartyReportingTool;
+        }
+        public void ShowReport(Report report)
+        {
+            //intention here is to enhance the internal reporting tool with new rendering capability 
+            //which is purchased from third party company :)
+            _thridPartyReportingTool.LoadReport(report.ReportName);
+            Console.WriteLine(_thridPartyReportingTool.FormatAndRenderReport(report.Data));       
+        }
+    }
+}
---
+++ Adapter/AdapterDemo/ThirdPartyLibraries/ThridPartyReportingTool.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdapterDemo.ThirdPartyLibraries
+{
+    public interface IThridPartyReportingTool
+    {
+        void LoadReport(string reportName);
+        string FormatAndRenderReport(List<string> data);
+    }
+
+    //Company purchased this repoting software which is third party software
+    public class ThridPartyReportingTool: IThridPartyReportingTool   //adaptee
+    {
+        private string _reportName = string.Empty;
+
+        public void LoadReport(string reportName)
+        {
+            _reportName = reportName;
+
+            //Find and Load Report template from Database
+        }
+
+        public string FormatAndRenderReport(List<string> data)
+        {
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("***************************************************************************");
+            sb.AppendLine($"*************{_reportName}******************************************");
+            data.ForEach(x =>
+            {
+                sb.AppendLine($"{x}");
+            });
+            sb.AppendLine("***************************************************************************");
+
+            return sb.ToString();
+        }
+    }
+}
